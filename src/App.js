f4d1252a67e508464410a179400d4d5065d99bc8
krishna <krishna404@gmail.com>
2022-04-05 18:40:44 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,90 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import {
+  HomeIcon,
+  UserIcon,
+  ChatIcon,
+  CameraIcon,
+  CogIcon,
+} from "@heroicons/react/outline";
 
 function App() {
+  const iconClasses = "h-24 w-24 rounded-full p-6";
+  const [links, setLinks] = useState([
+    {
+      title: "Home",
+      Icon: () => <HomeIcon className={iconClasses} />,
+      isActive: true,
+    },
+    {
+      title: "Profile",
+      Icon: () => <UserIcon className={iconClasses} />,
+      isActive: false,
+    },
+    {
+      title: "Message",
+      Icon: () => <ChatIcon className={iconClasses} />,
+      isActive: false,
+    },
+    {
+      title: "Photos",
+      Icon: () => <CameraIcon className={iconClasses} />,
+      isActive: false,
+    },
+    {
+      title: "Settings",
+      Icon: () => <CogIcon className={iconClasses} />,
+      isActive: false,
+    },
+  ]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-gray-800 text-gray-700 min-h-screen min-w-screen flex items-center justify-center">
+      <div className="bg-gray-50 px-12 py-2 rounded-xl h-28">
+        <ul className="flex flex-row items-center gap-12 relative">
+          {links.map((link, index) => (
+            <li key={index}>
+              <a
+                onClick={(e) => {
+                  setLinks(
+                    links.map((l) => {
+                      if (l.title === link.title) {
+                        return {
+                          ...l,
+                          isActive: true,
+                        };
+                      }
+                      return {
+                        ...l,
+                        isActive: false,
+                      };
+                    })
+                  );
+                }}
+                className={`flex flex-col gap-4 items-center ${
+                  link.isActive && "-translate-y-16"
+                } transition ease-in-out duration-600 group focus:outline-none`}
+                href="#!"
+              >
+                <div
+                  className={`${
+                    link.isActive &&
+                    "bg-emerald-400 ring-10 ring-gray-800 rounded-full"
+                  }`}
+                >
+                  {<link.Icon />}
+                </div>
+                <span
+                  className={`text-center text-lg opacity-0 ${
+                    link.isActive && "opacity-100"
+                  }`}
+                >
+                  {link.title}
+                </span>
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      ringWidth: {
+        10: "10px",
+      },
+    },
+  },
+  plugins: [],
+};
